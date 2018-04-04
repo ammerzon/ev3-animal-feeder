48ded07857d24d46e7d809b06de149bcdd711a9e
Andreas Kaltenleitner <kaltenleitner.andreas@gmail.com>
2018-04-04 22:57:09 +0200

    Add robot properties for movement

--- src/main/kotlin/com/angrynerds/ev3/core/FeederRobot.kt
+++ src/main/kotlin/com/angrynerds/ev3/core/FeederRobot.kt
@@ -5,6 +5,8 @@ import lejos.hardware.port.SensorPort
 import lejos.hardware.sensor.EV3ColorSensor
 import lejos.hardware.sensor.EV3IRSensor
 import lejos.hardware.sensor.EV3UltrasonicSensor
+import lejos.robotics.chassis.WheeledChassis
+import lejos.robotics.navigation.MovePilot
 
 object FeederRobot {
     var tractionMotorRight = Motor.D
@@ -15,4 +17,9 @@ object FeederRobot {
     var colorSensorRight = EV3ColorSensor(SensorPort.S2)
     var colorSensorForward = EV3ColorSensor(SensorPort.S4)
     var ultrasonicSensor = EV3UltrasonicSensor(SensorPort.S3)
+
+    var wheelRight = WheeledChassis.modelWheel(tractionMotorRight, 35.0).offset(172.5)
+    var wheelLeft = WheeledChassis.modelWheel(tractionMotorLeft, 35.0).offset(-172.5)
+    var chassis = WheeledChassis(arrayOf(wheelRight, wheelLeft), WheeledChassis.TYPE_DIFFERENTIAL)
+    var movePilot = MovePilot(chassis)
 }
