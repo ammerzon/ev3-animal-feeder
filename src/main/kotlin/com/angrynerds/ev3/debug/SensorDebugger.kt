ee2ecd2a14ecc79bb8a22c3383afff095973aa37
Patrick Mathae <matpat996@gmail.com>
2018-04-05 14:46:57 +0200

    Add auto refresh with buttons

--- src/main/kotlin/com/angrynerds/ev3/debug/SensorDebugger.kt
+++ src/main/kotlin/com/angrynerds/ev3/debug/SensorDebugger.kt
@@ -10,13 +10,19 @@ import kotlin.concurrent.thread
 
 class SensorDebugger : HttpHandler {
     override fun handle(exchange: HttpExchange?) {
-        val autoRefreshScript = "<meta http-equiv=\"refresh\" content=\"2\" />"
-        val response = "<html>$autoRefreshScript" +
+        val autoRefreshScript = "<html>" +
+                "<head>" +
+                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js\"></script>" +
+                "<script type=\"text/javascript\"> var timeout; \$(document).ready(function () { startReload(); }); function stopReload() { clearTimeout(timeout); } function startReload() { timeout = setTimeout(function() { window.location.reload(); }, 2000); } </script></head>"
+        val response = autoRefreshScript +
+                "<body>" +
                 "<b>Infrared:</b> ${FeederRobot.infraredSensor.measureDistance()}<br/>" +
                 "<b>Ultrasonic:</b> ${FeederRobot.ultrasonicSensor.measureDistance()}<br/>" +
                 "<b>Right Color:</b> ${FeederRobot.colorSensorRight.sampleSize()}<br/>" +
                 "<b>Forward Color:</b> ${FeederRobot.colorSensorForward.sampleSize()}" +
-                "</html>"
+                "<br /><button onclick=\"stopReload();\">Stop auto refresh!</button>" +
+                "<button onclick=\"startReload();\">Start auto refresh!</button>" +
+                "</body></html>"
         exchange?.sendResponseHeaders(200, response.length.toLong())
         val output = exchange?.responseBody
         output?.write(response.toByteArray())
@@ -26,10 +32,13 @@ class SensorDebugger : HttpHandler {
     companion object {
         fun startDebugServer() {
             thread {
-                val server = HttpServer.create(InetSocketAddress(8080), 0)
+                val port = 8081
+
+                val server = HttpServer.create(InetSocketAddress(port), 0)
                 server.createContext("/sensors", SensorDebugger())
                 server.executor = null
                 server.start()
+                println("Server startet on port $port")
             }
         }
     }
