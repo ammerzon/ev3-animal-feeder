529eaa09af926b039de98e93eb9d97be6b51336b
Simon Ammer <ammersimon@gmail.com>
2018-05-02 00:49:31 +0200

    Add avoid enclosure task

--- src/main/kotlin/com/angrynerds/ev3/Main.kt
+++ src/main/kotlin/com/angrynerds/ev3/Main.kt
@@ -1,6 +1,7 @@
 package com.angrynerds.ev3
 
 import com.angrynerds.ev3.core.FeederRobot
+import com.angrynerds.ev3.core.RxFeederRobot
 import com.angrynerds.ev3.extensions.getDistance
 import io.reactivex.Observable
 import lejos.hardware.Button
@@ -10,14 +11,20 @@ import lejos.sensors.RxEV3UltrasonicSensor
 
 const val OBSTACLE_MAX_DISTANCE = 35
 const val GRABBER_MAX_ROTATION = 15000
-const val ULTRASONIC_TABLE_DISTANCE = 0.031000002
+const val ULTRASONIC_LOW_TABLE_DISTANCE = 0.031000002
+const val ULTRASONIC_HIGH_TABLE_DISTANCE = 0.05
 const val GRABBER_UPWARD_SPEED = 360
 const val GRABBER_DOWNWARD_SPEED = 180
+const val FORWARD_SPEED = 35.0
+
+var isUltrasonicRaised = false
 
 fun main(args: Array<String>) {
 
+
     println("Initialize robot ...")
-    FeederRobot.infraredSensor
+    FeederRobot.tractionMotorLeft
+    RxFeederRobot.colorSensorForward
     resetToStartPosition()
     println("Press a button to continue ...")
     Button.waitForAnyPress()
@@ -29,15 +36,37 @@ fun main(args: Array<String>) {
     Button.waitForAnyPress()
 
     //SensorDebugger.startDebugServer()
-    raiseGrabber()
-    testAvoidThings()
 
+    // Avoid things
+    /*raiseGrabber()
+    testAvoidThings()*/
+
+    // Detect enclosure
+    testAvoidEnclosure()
+    
     Button.waitForAnyPress()
     FeederRobot.close()
 }
 
+fun testAvoidEnclosure() {
+    FeederRobot.movePilot.linearSpeed = FORWARD_SPEED
+    FeederRobot.movePilot.forward()
+    RxFeederRobot.ultrasonicSensor.distance.subscribe { distance ->
+        if ((isUltrasonicRaised && distance > ULTRASONIC_HIGH_TABLE_DISTANCE) || (!isUltrasonicRaised && distance > ULTRASONIC_LOW_TABLE_DISTANCE)) {
+            FeederRobot.movePilot.stop()
+            FeederRobot.movePilot.travel(-10.0)
+            FeederRobot.movePilot.rotate(180.0)
+        }
+        Thread.sleep(2000)
+        if (!FeederRobot.movePilot.isMoving) {
+            FeederRobot.movePilot.forward()
+        }
+    }
+
+}
+
 fun testAvoidThings() {
-    FeederRobot.movePilot.linearSpeed = 35.0
+    FeederRobot.movePilot.linearSpeed = FORWARD_SPEED
     FeederRobot.movePilot.forward()
     obstacles().subscribe { x -> onObstacle(x) }
 }
@@ -64,10 +93,12 @@ fun raiseGrabber() {
     FeederRobot.grabMotor.speed = GRABBER_UPWARD_SPEED
     FeederRobot.grabMotor.rotate(GRABBER_MAX_ROTATION - FeederRobot.grabberPositionAsAngle)
     FeederRobot.grabberPositionAsAngle = GRABBER_MAX_ROTATION
+    isUltrasonicRaised = true
 }
 
 fun resetToStartPosition() {
     FeederRobot.grabMotor.speed = GRABBER_DOWNWARD_SPEED
-    RxEV3UltrasonicSensor(FeederRobot.ultrasonicSensor, false).distance.filter({ it < ULTRASONIC_TABLE_DISTANCE }).take(1).subscribe({ FeederRobot.grabMotor.stop() })
+    RxEV3UltrasonicSensor(FeederRobot.ultrasonicSensor, false).distance.filter({ it < ULTRASONIC_LOW_TABLE_DISTANCE }).take(1).subscribe({ FeederRobot.grabMotor.stop() })
     FeederRobot.grabMotor.backward()
+    isUltrasonicRaised = false
 }
