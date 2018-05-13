1210c9bb2387ada1da7f02c40b71791bba243f32
Simon Ammer <ammersimon@gmail.com>
2018-05-13 08:06:56 +0200

    Add alpha test day tests

--- src/main/kotlin/com/angrynerds/ev3/Main.kt
+++ src/main/kotlin/com/angrynerds/ev3/Main.kt
@@ -2,74 +2,133 @@ package com.angrynerds.ev3
 
 import com.angrynerds.ev3.core.FeederRobot
 import com.angrynerds.ev3.core.RxFeederRobot
-import com.angrynerds.ev3.extensions.getDistance
-import io.reactivex.Observable
 import lejos.hardware.Button
-import lejos.sensors.ColorId
-import lejos.sensors.RxEV3ColorSensor
-import lejos.sensors.RxEV3IRSensor
-import lejos.sensors.RxEV3UltrasonicSensor
+import java.util.logging.Logger
 
-const val OBSTACLE_MAX_DISTANCE = 35
-const val GRABBER_MAX_ROTATION = 15000
-const val ULTRASONIC_LOW_TABLE_DISTANCE = 0.031000002
-const val ULTRASONIC_HIGH_TABLE_DISTANCE = 0.2
-const val GRABBER_UPWARD_SPEED = 400
-const val GRABBER_DOWNWARD_SPEED = 200
-const val FORWARD_SPEED = 35.0
-
-var isUltrasonicRaised = false
-var foodColor = ColorId.YELLOW
+val logger = Logger.getLogger("main")!!
 
 fun main(args: Array<String>) {
 
+    logger.info("Animal feeder started")
+    openConnections()
+    resetToInitialState()
 
-    println("Initialize robot ...")
-    FeederRobot.tractionMotorLeft
-    RxFeederRobot.rxColorSensorForward
-    resetToStartPosition()
-    println("Press a button to continue ...")
+    logger.info("Press a button to calibrate the robot...")
+    Button.waitForAnyPress()
+    calibrateRobot()
+
+    logger.info("Press a button to start execution...")
     Button.waitForAnyPress()
 
-    initLogging()
-    Button.waitForAnyPress()
-
-    //SensorDebugger.startDebugServer()
-
-    // Avoid things
-    raiseGrabber()
-    testAvoidThings()
-
-    // Avoid enclosure
-    testAvoidEnclosure()
-
-    // Raise stone
-    testRaiseStone()
-
+    logger.info("Press a button to close the program...")
     Button.waitForAnyPress()
     FeederRobot.close()
 }
 
-fun initLogging() {
+fun resetToInitialState() {
+    logger.info("Resetting to initial state")
+    TODO("not implemented")
+}
+
+fun calibrateRobot() {
+    logger.info("Robot calibration started")
+    TODO("not implemented")
+    logger.info("Animal type: " + FeederRobot.animalType)
+}
+
+fun openConnections() {
+    logger.info("Open connections")
+    FeederRobot.tractionMotorLeft
+    RxFeederRobot.rxColorSensorForward
+}
+
+fun initializeLogging() {
+    logger.info("Initialize sensor values logging")
     RxFeederRobot.rxColorSensorForward.colorId.subscribe({ println("Color forward: " + it) })
     RxFeederRobot.rxColorSensorRight.colorId.subscribe({ println("Color right: " + it) })
     RxFeederRobot.rxInfraredSensor.distance.subscribe({ println("IR: " + it) })
     RxFeederRobot.rxUltrasonicSensor.distance.subscribe({ println("US: " + it) })
 }
 
-fun testAvoidEnclosure() {
+/**
+ * Roboter fährt auf Tischkante zu, erkennt Abgrund, wendet und fährt weiter.
+ */
+fun test01PrecepiceDetection() {
+    TODO("not implemented")
+}
+
+/**
+ * Roboter fährt auf Stein zu und stoppt kurz vor Berührung des Steines ab.
+ */
+fun test02ObstacleStop() {
+    TODO("not implemented")
+}
+
+/**
+ * Duplostein befindet sich direkt vor Roboter, Roboter erkennt Farbe des Futter und öffnet Greifarm.
+ */
+fun test03FeedColorDetection() {
+    TODO("not implemented")
+}
+
+/**
+ * Roboter schließt Greifarm und hebt Duplostein, hält ihn oben und kann ihn transportieren.
+ */
+fun test04CarryStone() {
+    TODO("not implemented")
+}
+
+/**
+ * Roboter fährt (mit gehobenen/geschlossenen) Greifarm auf kalkulierte Richtung des Geheges zu.
+ */
+fun test05FindStable() {
+    TODO("not implemented")
+}
+
+/**
+ * Baum befindet sich sich ca. 20 cm vor Roboter. Dieser erkennt Baum und schmeißt ihn um.
+ */
+fun test06CutTree() {
+    TODO("not implemented")
+}
+
+/**
+ * Roboter befindet sich in der Arena und stößt Zaun weg.
+ */
+fun test07DemolishFence() {
+    TODO("not implemented")
+}
+
+/**
+ * Dem Roboter werden unterschiedliche, relevante, Hindernisse (wie zB Baum, Zaun, Futter) vorgesetzt. Der Roboter reagiert anhand des vorgesetzten Hindernisses.
+ */
+fun test08ObstacleDetection() {
+    TODO("not implemented")
+}
+
+/**
+ * Der Roboter steht mit erhobenem Arm vor dem Gehege und fährt darauf zu. Wenn der Arm über dem Gehege ist soll es stoppen.
+ */
+fun test09MeasureHeight() {
+    TODO("not implemented")
+}
+
+/*fun testAvoidEnclosure() {
     print("Started testAvoidEnclosure")
     FeederRobot.movePilot.linearSpeed = FORWARD_SPEED
     FeederRobot.movePilot.forward()
     RxFeederRobot.rxUltrasonicSensor.distance.subscribe { distance ->
-        if ((isUltrasonicRaised && distance > ULTRASONIC_HIGH_TABLE_DISTANCE) || (!isUltrasonicRaised && distance > ULTRASONIC_LOW_TABLE_DISTANCE)) {
-            FeederRobot.movePilot.stop()
-            FeederRobot.movePilot.travel(-100.0)
-            FeederRobot.movePilot.rotate(180.0)
-        }
-        Thread.sleep(2000)
-        if (!FeederRobot.movePilot.isMoving) {
-            FeederRobot.movePilot.forward()
+        print("UR: " + distance)
+        if (distance.isFinite()) {
+            if ((isUltrasonicRaised && distance > ULTRASONIC_HIGH_TABLE_DISTANCE) || (!isUltrasonicRaised && distance > ULTRASONIC_LOW_TABLE_DISTANCE)) {
+                FeederRobot.movePilot.stop()
+                FeederRobot.movePilot.travel(-100.0)
+                FeederRobot.movePilot.rotate(90.0)
+            }
+            Thread.sleep(2000)
+            if (!FeederRobot.movePilot.isMoving) {
+                FeederRobot.movePilot.forward()
+            }
         }
     }
 }
@@ -99,29 +158,17 @@ fun obstacles(): Observable<Float> {
             .filter { distance -> distance <= OBSTACLE_MAX_DISTANCE }
 }
 
-fun raiseGrabber() {
-    FeederRobot.grabMotor.speed = GRABBER_UPWARD_SPEED
-    FeederRobot.grabMotor.rotate(GRABBER_MAX_ROTATION - FeederRobot.grabberPositionAsAngle)
-    FeederRobot.grabberPositionAsAngle = GRABBER_MAX_ROTATION
-    isUltrasonicRaised = true
-}
-
-fun resetToStartPosition() {
-    FeederRobot.grabMotor.speed = GRABBER_DOWNWARD_SPEED
-    FeederRobot.grabMotor.backward()
-    RxEV3UltrasonicSensor(FeederRobot.ultrasonicSensor, false).distance.filter({ it < ULTRASONIC_LOW_TABLE_DISTANCE }).take(1).subscribe({ FeederRobot.grabMotor.stop() })
-    isUltrasonicRaised = false
-}
-
 fun testRaiseStone() {
-    print("Started testRaiseStone")
+    println("Started testRaiseStone")
+    println("Place stone and press a button ...")
     Button.waitForAnyPress()
     val forwardColorId = RxFeederRobot.rxColorSensorForward.colorId.blockingFirst()
     if (forwardColorId.equals(foodColor)) {
         println("Food recognized color=${forwardColorId.name}")
-        raiseGrabber()
+        FeederRobot.grabMotor.speed = GRABBER_UPWARD_SPEED + 200
+        FeederRobot.grabMotor.rotate(24000)
     } else {
         println("No food recognized color=${forwardColorId.name}")
         FeederRobot.movePilot.travel(-50.0)
     }
-}
+}*/
---
+++ src/main/kotlin/com/angrynerds/ev3/util/Constants.kt
@@ -0,0 +1,39 @@
+package com.angrynerds.ev3.util
+
+import lejos.sensors.ColorId
+
+object Constants {
+    object Calibration {
+        const val WAITING_TIME = 2
+    }
+
+    object PrecipiceDetection {
+        // vertical distance between the floor and the ultrasonic sensor when the grabber position is up
+        const val ULTRASONIC_THRESHOLD_GRABBER_UP = 0.2
+        // vertical distance between the floor and the ultrasonic sensor when the grabber position is down
+        const val ULTRASONIC_THRESHOLD_GRABBER_DOWN = 0.031000002
+    }
+
+    object ObstacleCheck {
+        // color of the tree trunk
+        val TREE_COLOR = ColorId.BROWN
+        // color of Winnie Pooh's feed color
+        val WINNIE_POOH_FEED_COLOR = ColorId.YELLOW
+        // color of I-AahÄs feed color
+        val I_AAH_FEED_COLOR = ColorId.GREEN
+        // colors of the other animals placed in the arena (e. g. tiger, ...)
+        val ANIMAL_COLORS = arrayOf(ColorId.BLACK, ColorId.WHITE, ColorId.BLUE, ColorId.RED, ColorId.WHITE)
+    }
+
+    object DemolishFence {
+        // vertical distance between the fence and the ultrasonic sensor
+        val ULTRASONIC_THRESHOLD = 10
+    }
+
+    object Grabber {
+        const val UPWARD_SPEED = 400
+        const val DOWNWARD_SPEED = 200
+        // rotations needed to raise the grabber
+        const val GRAB_MOTOR_MAX_ROTATIONS = 1800
+    }
+}
